63fe03e1e0c0192c54b3b7bf95a61b1414530d1b
s22540 <s22540@pjwstk.edu.pl>
2024-03-23 17:07:07 +0100

    Modyfikacja 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("To jest kod dodany z pierwszego commita.");
+
+for (int i = 1; i <= 5; i++)
+{
+    Console.WriteLine(i);
+}
