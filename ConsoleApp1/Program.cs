8f1596b466fe6daf8acc3732ac411d4500fe1ac0
s22540 <s22540@pjwstk.edu.pl>
2024-03-23 17:21:29 +0100

    Dodanie funkcji wyszukującej maksymalną wartość liczby

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,11 @@ namespace SredniaLiczb
             int[] liczby = { 1, 2, 3, 4, 5 };
             double srednia = ObliczSrednia(liczby);
             Console.WriteLine($"Średnia: {srednia}");
+
+            int[] liczbyMax = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 0 };
+            int maksimum = ZnajdzMaksimum(liczbyMax);
+            Console.WriteLine($"Maksymalna wartość: {maksimum}");
+
             Console.WriteLine("Naciśnij dowolny klawisz, aby zamknąć...");
             Console.ReadKey();
         }
@@ -29,6 +34,25 @@ namespace SredniaLiczb
 
             return suma / liczby.Length;
 
+        }
+
+        public static int ZnajdzMaksimum(int[] liczby)
+        {
+            if (liczby == null || liczby.Length == 0)
+            {
+                throw new ArgumentException("Tablica nie może być null lub pusta.");
+            }
+
+            int maks = liczby[0];
+            foreach (int liczba in liczby)
+            {
+                if (liczba > maks)
+                {
+                    maks = liczba;
+                }
+            }
+
+            return maks;
         }
 
     }
